585750b0dc20089195dc2624d9d4166dcfd1635b
nisargkolhe <nisargkolhe@gmail.com>
2016-04-13 00:39:14 +0530

    Implemented Screens

---
+++ core/src/com/projectadmob/screens/GameScreen.java
@@ -0,0 +1,49 @@
+package com.projectadmob.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by nisarg on 13/4/16.
+ */
+public class GameScreen implements Screen {
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
+        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
+        // Fills the screen with the selected color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
