8acaddd08ff4baa94c7b6771cd9081c3eb7cbfef
nisargkolhe <nisargkolhe@gmail.com>
2016-04-12 23:39:41 +0530

    First Commit

---
+++ desktop/src/com/projectadmob/toastybread/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.projectadmob.toastybread.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.projectadmob.toastybread.TBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TBGame(), config);
+	}
+}
---
+++ html/src/com/projectadmob/toastybread/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.projectadmob.toastybread.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.projectadmob.toastybread.TBGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TBGame();
+        }
+}
